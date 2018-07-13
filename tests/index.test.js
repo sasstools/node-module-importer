9bedaff1e14deae9a94e320469ae5be16a9ac68d
xzyfer <xzyfer@gmail.com>
2018-07-14 02:17:16 +1000

    Stop using snapshots for testing

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,38 +1,40 @@
 const sass = require('node-sass');
 const importer = require('../');
 
+const foundation = sass.renderSync({
+  file: 'node_modules/foundation/scss/foundation.scss',
+}).css.toString();
+
 const compile = function(data) {
   return new Promise((yeah, nah) => {
     return sass.render(
       { data, importer: importer },
-      (err, results) => err ? nah(err) : yeah(results),
+      (err, results) => err ? nah(err) : yeah(results.css.toString()),
     );
   });
 }
 
 describe('node-module-importer', () => {
-  it('should resolve Sass @import from npm packages', () => (
-    compile('@import "foundation/scss/foundation.scss"')
-      .then(results => expect(results.css.toString()).toMatchSnapshot())
-  ));
-  it('should resolve Sass @import without extension from npm packages', () => (
-    compile('@import "foundation/scss/foundation.scss"')
-      .then(results => expect(results.css.toString()).toMatchSnapshot())
-  ));
-  it('should resolve Sass @import for partials from npm packages', () => (
-    compile('@import "foundation/scss/foundation/_variables.scss"')
-      .then(results => expect(results.css.toString()).toMatchSnapshot())
-  ));
-  it('should resolve Sass @import for partials without extension from npm packages', () => (
-    compile('@import "foundation/scss/foundation/_variables"')
-      .then(results => expect(results.css.toString()).toMatchSnapshot())
-  ));
-  it('should resolve Sass @import for partials without underscore from npm packages', () => (
-    compile('@import "foundation/scss/foundation/variables.scss"')
-      .then(results => expect(results.css.toString()).toMatchSnapshot())
-  ));
-  it('should resolve Sass @import for partials without underscore and extension from npm packages', () => (
-    compile('@import "foundation/scss/foundation/variables"')
-      .then(results => expect(results.css.toString()).toMatchSnapshot())
-  ));
+  describe('async', () => {
+    it('should resolve Sass @import from npm packages', () => (
+      compile('@import "foundation/scss/foundation.scss"')
+        .then(result => expect(result === foundation).toBeTruthy())
+    ));
+    it('should resolve Sass @import without extension from npm packages', () => (
+      compile('@import "foundation/scss/foundation.scss"')
+        .then(result => expect(result === foundation).toBeTruthy())
+    ));
+    it('should resolve Sass @import for partials from npm packages', () => (
+      compile('@import "foundation/scss/foundation/_variables.scss"')
+    ));
+    it('should resolve Sass @import for partials without extension from npm packages', () => (
+      compile('@import "foundation/scss/foundation/_variables"')
+    ));
+    it('should resolve Sass @import for partials without underscore from npm packages', () => (
+      compile('@import "foundation/scss/foundation/variables.scss"')
+    ));
+    it('should resolve Sass @import for partials without underscore and extension from npm packages', () => (
+      compile('@import "foundation/scss/foundation/variables"')
+    ));
+  });
 });
