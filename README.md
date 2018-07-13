9a0b6f6f0dc3be54d1436d1a83288ec86c10f7ef
xzyfer <xzyfer@gmail.com>
2018-07-13 22:54:46 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# node-module-importer
+
+>Node Sass importer for npm packages
+
+## Install
+
+This package has a peer dependency on Node Sass for ensure import API compatibility.
+
+```sh
+npm install @node-sass/node-module-importer node-sass
+```
+
+## Usage
+
+Install an npm package with the Sass files you want to import.
+```js
+npm install foundation
+```
+
+When Node Sass parses an `@import` in will try to match the first part of the URL with an installed npm package. The rest of the URL will be resolved relative to the where the package is installed.
+
+```css
+@import "foundation/scss/foundation.scss";
+```
+
+### Node Sass API
+
+```js
+var sass = require('node-sass');
+var nodeModuleImport = require('@node-sass/node-module-importer');
+
+sass.render({
+  file: 'index.scss',
+  importer: [nodeModuleImport],
+}, function (err, result) {
+  if (err) throw err;
+  console.log(result.css.toString());
+});
+```
+
+### Node Sass CLI
+
+```sh
+$ node-sass index.scss --importer node_modules/node-module-importer/index.js
+```
+
+## FAQ
+
+### Why is this different from adding `node_modules` to `includePaths`?
+
+npm can install packages in nested `node_modules` folders i.e. `node_modules/package_1/node_modules/package_2`.
---
+++ index.js
@@ -0,0 +1,67 @@
+var fs = require('fs');
+var path = require('path');
+var exts = ['.sass', '.scss'];
+
+function exists(file) {
+  try {
+    fs.accessSync(file, fs.constants.F_OK);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+module.exports = function(url, prev, done) {
+  if (!url) return done(null);
+
+  var urlParts = url.split('/');
+  var packageName = urlParts[0];
+
+  try {
+    var packagePath = require.resolve(packageName, { paths: [process.cwd()] });
+  } catch (e) {
+    return done(null);
+  }
+
+  if (!packagePath) return done(null);
+
+  var parts = packagePath.split(path.sep);
+
+  for (var i = parts.length; i >= 0; i--) {
+    if (parts[i] !== packageName || parts[i - 1] === packageName) continue;
+
+    var before = parts.splice(0, i + 1);
+    var after = urlParts.splice(1);
+
+    var resolved = [].concat(before, after).join(path.sep);
+    var relative = path.relative(process.cwd(), resolved);
+    var ext = path.extname(relative);
+    var basename = path.basename(relative, ext);
+
+    console.log({ resolved, relative, basename, ext })
+
+    if (ext) {
+      if (basename[0] === '_') {
+        if (exists(relative)) return done({ file: relative });
+      } else {
+        if (exists(relative)) return done({ file: relative });
+        var partial = relative.split(path.sep).splice(-1, 1, '_' + basename).join(path.sep);
+        if (exists(partial)) return done({ file: partial });
+      }
+    } else {
+      for (var j = 0; j < exts.length; j++) {
+        if (basename[0] === '_') {
+          if (exists(relative + exts[j])) return done({ file: relative + exts[j] });
+        } else {
+          if (exists(relative + exts[j])) return done({ file: relative + exts[j] });
+          var partial = relative.split(path.sep);
+          partial.splice(-1, 1, '_' + basename);
+          partial = partial.join(path.sep);
+          if (exists(partial + exts[j])) return done({ file: partial + exts[j] });
+        }
+      }
+    }
+  }
+
+  return done(null);
+};
