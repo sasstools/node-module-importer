f537fe876601e45630b0e86f222794df6f03c04f
xzyfer <xzyfer@gmail.com>
2018-07-14 13:38:29 +1000

    test

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,10 +3,10 @@ const sass = require('node-sass');
 const importer = require('../');
 
 const foundation = sass.renderSync({
-  file: 'node_modules/foundation/scss/foundation.scss',
+  file: path.join('node_modules', 'foundation', 'scss', 'foundation.scss'),
 }).css.toString();
 
-const normalize = `@import url(node_modules${path.sep}normalize.css${path.sep}normalize.css);\n`;
+const normalize = `@import url(${path.join('node_modules' ,'normalize.css' ,'normalize.css')});\n`;
 
 const compile = function(data) {
   return new Promise((yeah, nah) => {
@@ -32,27 +32,27 @@ describe('node-module-importer', () => {
   [[ 'async', compile ], [ 'sync', compileSync ]].forEach(([ label, func ]) => {
     describe(label, () => {
       it('should resolve Sass @import from npm packages', () => (
-        func('@import "foundation/scss/foundation.scss"')
+        func(`@import "${path.join('foundation', 'scss', 'foundation.scss')}"`)
           .then(result => expect(result === foundation).toBeTruthy())
       ));
       it('should resolve Sass @import without extension from npm packages', () => (
-        func('@import "foundation/scss/foundation.scss"')
+        func(`@import "${path.join('foundation', 'scss', 'foundation.scss')}"`)
           .then(result => expect(result === foundation).toBeTruthy())
       ));
       it('should resolve Sass @import for partials from npm packages', () => (
-        func('@import "foundation/scss/foundation/_variables.scss"')
+        func(`@import "${path.join('foundation', 'scss', 'foundation', '_variables.scss')}"`)
       ));
       it('should resolve Sass @import for partials without extension from npm packages', () => (
-        func('@import "foundation/scss/foundation/_variables"')
+        func(`@import "${path.join('foundation', 'scss', 'foundation', '_variables')}"`)
       ));
       it('should resolve Sass @import for partials without underscore from npm packages', () => (
-        func('@import "foundation/scss/foundation/variables.scss"')
+        func(`@import "${path.join('foundation', 'scss', 'foundation', 'variables.scss')}"`)
       ));
       it('should resolve Sass @import for partials without underscore and extension from npm packages', () => (
-        func('@import "foundation/scss/foundation/variables"')
+        func(`@import "${path.join('foundation', 'scss', 'foundation', 'variables')}"`)
       ));
       it('should resolve Sass @import for npm packages that look like Sass files', () => (
-        func('@import "normalize.css/normalize.css"')
+        func(`@import "${path.join('normalize.css', 'normalize.css')}"`)
           .then(result => expect(result === normalize).toBeTruthy())
       ));
     });
